a4113851f50af9eb1e3a9d51a09fba0b6bdf76b9
TayiebK <150594024+mtk2608@users.noreply.github.com>
2024-02-26 11:02:07 +0200

    Final Version

---
+++ JsonAssignment2.0/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonAssignment2._0
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            NewUser = LoadData();
+
+            
+        }
+
+        #region Properties
+
+        private Profile _profile;
+
+        public Profile NewUser
+        {
+            get { return _profile; }
+            set
+            {
+                _profile = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region EventHandlers
+
+        public void SaveData(Profile profile)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LocalStorageText.txt");
+            string profileJson = JsonConvert.SerializeObject(profile);
+            File.WriteAllText(filePath, profileJson);
+
+            TextBox.Text = profileJson;
+        }
+
+
+        public Profile LoadData()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LocalStorageText.txt");
+
+            if (File.Exists(filePath))
+            {
+
+                string fileContent = File.ReadAllText(filePath);
+
+                Profile savedProfile = JsonConvert.DeserializeObject<Profile>(fileContent);
+
+                return savedProfile;
+            }
+            else
+                return new Profile();
+        }
+
+
+        private void Save_Clicked(object sender, EventArgs e)
+        {
+            SaveData(NewUser);
+        }
+
+        private void Load_Clicked(object sender, EventArgs e)
+        {
+            NewUser = LoadData();
+        }
+        #endregion
+
+    }
+}
+
---
+++ JsonAssignment2.0/Profile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAssignment2._0
+{
+     public class Profile : INotifyPropertyChanged
+    {
+
+        private string username;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Username)));
+            }
+        }
+
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Surname)));
+            }
+        }
+
+        private string emailAddress;
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set
+            {
+                emailAddress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EmailAddress)));
+            }
+        }
+
+        private string bio;
+        public string Bio
+        {
+            get { return bio; }
+            set
+            {
+                bio = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bio)));
+            }
+        }
+    }
+}
